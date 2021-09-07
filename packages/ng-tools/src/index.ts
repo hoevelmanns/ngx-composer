6eb4b42699a6f8b097667d3f49ef989cd66bcb3a
hoevelmanns <mail@hoevelmanns.io>
2021-09-08 00:17:46 +0200

    removed creation of app-loader from ng-tools

--- packages/ng-tools/src/index.ts
+++ packages/ng-tools/src/index.ts
@@ -1,8 +1,5 @@
 import { autoInjectable, singleton } from 'tsyringe'
 import execa from 'execa'
-import * as cheerio from 'cheerio'
-import { readFile, outputFile } from 'fs-extra'
-import { join } from 'path'
 import { lookpath } from 'lookpath'
 
 @autoInjectable()
@@ -11,7 +8,8 @@ export class NgCliService {
     private bin = 'ng'
 
     /**
-     * Creates new Angular Workspace
+     * Runs the creation of a new Angular Workspace
+     * @link https://angular.io/cli/new
      */
     async new(name: string, options?: { args: string[]; cwd?: string; options?: execa.Options }): Promise<void> {
         await execa(this.bin, ['new', name, ...(options?.args ?? [])], {
@@ -21,11 +19,9 @@ export class NgCliService {
     }
 
     /**
-     * Builds an angular application
+     * Runs the build of an angular application
      *
-     * @param {string[]} args
-     * @param {string} cwd
-     * @param {execa.Options} options
+     * @link https://angular.io/cli/build
      */
     async build(args: string[], cwd?: string, options?: execa.Options): Promise<void> {
         await execa(this.bin, ['build', ...args], {
@@ -35,7 +31,7 @@ export class NgCliService {
     }
 
     /**
-     * Serves an angular application
+     * Runs the serve process of an Angular application.
      *
      * @param {string[]} args
      * @param {string} cwd
@@ -65,35 +61,25 @@ export class NgCliService {
         })
     }
 
+    /**
+     * Removes the budget configuration
+     */
     async setUnlimitedBudget(appName: string, dir: string): Promise<void> {
         await this.config(`projects.${appName}.architect.build.configurations.production.budgets`, '[]', dir)
     }
 
+    /**
+     * Runs 'ng add' to install a package and run a schematic.
+     *
+     * @link https://angular.io/cli/add
+     */
     async add(pkgName: string, cwd: string): Promise<void> {
         await execa(this.bin, ['add', pkgName, '--skip-confirmation'], { cwd }).catch(null)
     }
 
     /**
-     * Creates a file containing only the dist scripts
-     *
-     * @param {string} outputPath - the output path
-     * @param {string} filename - the target filename
-     * @param {boolean} serve
+     * Installs dependencies
      */
-    async createLoaderFile(outputPath: string, filename: string, serve?: boolean): Promise<void> {
-        const encoding = 'utf8'
-        const scripts = <string[]>[]
-        const indexHtml = serve // todo create template
-            ? `<link rel="stylesheet" href="styles.css"><script src="runtime.js" defer></script><script src="polyfills.js" defer></script>
-             <script src="vendor.js" defer></script><script src="main.js" defer></script></body>`
-            : await readFile(join(outputPath, 'index.html'), { encoding })
-
-        const $ = cheerio.load(indexHtml)
-        $('script, link[rel="stylesheet"]').each((index, elem) => scripts.push($.html(elem)))
-
-        await outputFile(join(outputPath, filename), scripts.join('\n'), { encoding })
-    }
-
     async install(cwd: string, silent = true): Promise<void> {
         const pkgManager = await this.getPackageManager()
         await execa(pkgManager, ['install'], { cwd, stdio: silent ? 'ignore' : 'inherit' })
